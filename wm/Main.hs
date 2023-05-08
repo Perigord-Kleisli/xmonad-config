345a491a74b3c6530499cf36ecb1185cf0a9a35b
Trouble-Truffle <perigordtruffle7318@gmail.com>
2023-05-09 06:31:58 +0800

    feat: Remove borders in fullscreem

--- wm/Main.hs
+++ wm/Main.hs
@@ -6,10 +6,12 @@ module Main (main) where
 import System.Environment.XDG.BaseDir
 import XMonad
 import XMonad.Hooks.EwmhDesktops (ewmh, ewmhFullscreen)
+import XMonad.Hooks.ManageDocks (avoidStruts)
 import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.StatusBar (defToggleStrutsKey, statusBarProp, withEasySB)
 import XMonad.Hooks.StatusBar.PP
 import XMonad.Layout.Magnifier (magnifiercz')
+import XMonad.Layout.NoBorders (Ambiguity (OnlyFloat, OtherIndicated), lessBorders, noBorders)
 import XMonad.Layout.ThreeColumns (ThreeCol (ThreeColMid))
 import XMonad.Util.EZConfig (additionalKeysP)
 import XMonad.Util.Loggers (logTitles)
@@ -25,52 +27,51 @@ main = do
 
 myConfig =
   def
-    { modMask = mod4Mask, -- Rebind Mod to the Super key
-      layoutHook = myLayout, -- Use custom layouts
-      manageHook = myManageHook, -- Match on certain windows
-      terminal = "kitty",
-      startupHook = do
-        mempty
-        -- spawn "trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 10 --transparent true --tint 0x5f5f5f --height 18"
+    { modMask = mod4Mask -- Rebind Mod to the Super key
+    , layoutHook = lessBorders OtherIndicated $ avoidStruts myLayout -- Use custom layouts
+    , manageHook = myManageHook
+    , terminal = "kitty"
+    , focusedBorderColor = "#ff5555"
+    , normalBorderColor = "#bbbbbb"
     }
-    `additionalKeysP` [ ("M-p", spawn "rofi -show drun"),
-                        ("M-S-s", spawn "flameshot gui"),
-                        ("M-S-x", kill),
-                        ("<XF86AudioLowerVolume>", spawn "amixer -q sset Master 3%-"),
-                        ("<XF86AudioRaiseVolume>", spawn "amixer -q sset Master 3%+"),
-                        ("<XF86AudioMute>", spawn "amixer -q sset Master toggle")
+    `additionalKeysP` [ ("M-p", spawn "rofi -show drun")
+                      , ("M-S-s", spawn "flameshot gui")
+                      , ("M-S-x", kill)
+                      , ("<XF86AudioLowerVolume>", spawn "amixer -q sset Master 3%-")
+                      , ("<XF86AudioRaiseVolume>", spawn "amixer -q sset Master 3%+")
+                      , ("<XF86AudioMute>", spawn "amixer -q sset Master toggle")
                       ]
 
 myManageHook :: ManageHook
 myManageHook =
   composeAll
-    [ className =? "Gimp" --> doFloat,
-      isDialog --> doFloat
+    [ className =? "Gimp" --> doFloat
+    , isDialog --> doFloat
     ]
 
-myLayout = tiled ||| Mirror tiled ||| Full ||| threeCol
-  where
-    threeCol = magnifiercz' 1.3 $ ThreeColMid nmaster delta ratio
-    tiled = Tall nmaster delta ratio
-    nmaster = 1 -- Default number of windows in the master pane
-    ratio = 1 / 2 -- Default proportion of screen occupied by master pane
-    delta = 3 / 100 -- Percent of screen to increment by when resizing panes
+myLayout = tiled ||| Mirror tiled ||| noBorders Full ||| threeCol
+ where
+  threeCol = magnifiercz' 1.3 $ ThreeColMid nmaster delta ratio
+  tiled = Tall nmaster delta ratio
+  nmaster = 1 -- Default number of windows in the master pane
+  ratio = 1 / 2 -- Default proportion of screen occupied by master pane
+  delta = 3 / 100 -- Percent of screen to increment by when resizing panes
 
 myXmobarPP :: PP
 myXmobarPP =
   def
-    { ppSep = magenta " • ",
-      ppTitleSanitize = xmobarStrip,
-      ppCurrent = wrap " " "" . xmobarBorder "Top" "#8be9fd" 2,
-      ppHidden = white . wrap " " "",
-      ppHiddenNoWindows = lowWhite . wrap " " "",
-      ppUrgent = red . wrap (yellow "!") (yellow "!"),
-      ppOrder = \case [ws, l, _, wins] -> [ws, l, wins]; _ -> undefined,
-      ppExtras = [logTitles formatFocused formatUnfocused]
+    { ppSep = magenta " • "
+    , ppTitleSanitize = xmobarStrip
+    , ppCurrent = wrap " " "" . xmobarBorder "Top" "#8be9fd" 2
+    , ppHidden = white . wrap " " ""
+    , ppHiddenNoWindows = lowWhite . wrap " " ""
+    , ppUrgent = red . wrap (yellow "!") (yellow "!")
+    , ppOrder = \case [ws, l, _, wins] -> [ws, l, wins]; _ -> undefined
+    , ppExtras = [logTitles formatFocused formatUnfocused]
     }
-  where
-    formatFocused = wrap (white "[") (white "]") . magenta . ppWindow
-    formatUnfocused = wrap (lowWhite "[") (lowWhite "]") . blue . ppWindow
+ where
+  formatFocused = wrap (white "[") (white "]") . magenta . ppWindow
+  formatUnfocused = wrap (lowWhite "[") (lowWhite "]") . blue . ppWindow
 
 -- \| Windows should have *some* title, which should not not exceed a
 -- sane length.
