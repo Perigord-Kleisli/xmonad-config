dab2612b8e4ff36827166f93b9271e98400591ef
Trouble-Truffle <perigordtruffle7318@gmail.com>
2023-05-22 11:22:33 +0800

    feat(bar): switch bar from xmobar to taffybar

--- bar/Main.hs
+++ bar/Main.hs
@@ -1,73 +1,36 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main (main) where
 
-------------------------------------------------------------------------------
--- \|
--- Copyright: (c) 2018, 2019, 2022 Jose Antonio Ortega Ruiz
--- License: BSD3-style (see LICENSE)
---
--- Maintainer: jao@gnu.org
--- Stability: unstable
--- Portability: portable
--- Created: Sat Nov 24, 2018 21:03
---
---
--- An example of a Haskell-based xmobar. Compile it with
---   ghc --make -- xmobar.hs
--- with the xmobar library installed or simply call:
---   xmobar /path/to/xmobar.hs
--- and xmobar will compile and launch it for you and
-------------------------------------------------------------------------------
+import Control.Monad.IO.Class (MonadIO (liftIO))
+import Data.Default (def)
+import System.Taffybar.Information.CPU
+import System.Taffybar.Information.Network (getDeviceSamples)
+import System.Taffybar.SimpleConfig
+import System.Taffybar.Widget
+import System.Taffybar.Widget.Generic.Graph
+import System.Taffybar.Widget.Generic.PollingGraph
 
-import Xmobar
-
--- Example user-defined plugin
-
-data HelloWorld = HelloWorld
-  deriving (Read, Show)
-
-instance Exec HelloWorld where
-  alias HelloWorld = "hw"
-  run HelloWorld = return "<fc=red>xmo</fc>"
-
--- Configuration, using predefined monitors as well as our HelloWorld
--- plugin:
-
-config :: Config
-config =
-  defaultConfig
-    { font = "xft:Sans Mono-10"
-    , additionalFonts = []
-    , borderColor = "black"
-    , border = TopB
-    , bgColor = "black"
-    , fgColor = "grey"
-    , alpha = 255
-    , position = Top
-    , textOffset = -1
-    , iconOffset = -1
-    , lowerOnStart = True
-    , pickBroadest = False
-    , persistent = False
-    , hideOnStart = False
-    , iconRoot = "."
-    , allDesktops = True
-    , overrideRedirect = True
-    , textOutputFormat = Ansi
-    , commands =
-        [ Run $ Network "eth0" ["-L", "0", "-H", "32", "--normal", "green", "--high", "red"] 10
-        , Run $ Network "eth1" ["-L", "0", "-H", "32", "--normal", "green", "--high", "red"] 10
-        , Run $ Cpu ["-L", "3", "-H", "50", "--normal", "green", "--high", "red"] 10
-        , Run $ Memory ["-t", "Mem: <usedratio>%"] 10
-        , Run $ Swap [] 10
-        , Run $ Com "uname" ["-s", "-r"] "" 36000
-        , Run $ Date "%a %b %_d %Y %H:%M:%S" "date" 10
-        , Run $ BatteryP ["BAT0"] ["-t", "<acstatus><watts> (<left>%)", "-L", "10", "-H", "80", "-p", "3", "--", "-O", "<fc=green>On</fc> - ", "-i", "", "-L", "-15", "-H", "-5", "-l", "red", "-m", "blue", "-h", "green", "-a", "notify-send -u critical 'Battery running out!!'", "-A", "3"] 600
-        , Run XMonadLog
-        ]
-    , sepChar = "%"
-    , alignSep = "}{"
-    , template = "%XMonadLog% | %cpu% | %memory% * %swap% }{ <fc=#ee9a00>%date%</fc> | %battery% | %uname%"
-    }
+cpuCallback :: IO [Double]
+cpuCallback = do
+  (_, systemLoad, totalLoad) <- cpuLoad
+  return [totalLoad, systemLoad]
 
 main :: IO ()
-main = configFromArgs config >>= xmobar
+main = do
+  let cpuCfg =
+        def
+          { graphDataColors = [(0, 1, 0, 1), (1, 0, 1, 0.5)]
+          , graphLabel = Just "cpu"
+          }
+      netCfg = def{graphLabel = Just "net"}
+      clock = textClockNewWith def
+      cpu = pollingGraphNew cpuCfg 0.5 cpuCallback
+      network = networkGraphNew netCfg Nothing
+      workspaces = workspacesNew def
+      simpleConfig =
+        def
+          { startWidgets = [workspaces]
+          , endWidgets = [clock, sniTrayNew, cpu, network]
+          }
+  simpleTaffybar simpleConfig
--- wm/Main.hs
+++ wm/Main.hs
@@ -1,28 +1,50 @@
-{-# LANGUAGE LambdaCase #-}
 {-# OPTIONS_GHC -Wno-missing-signatures #-}
 
 module Main (main) where
 
+import qualified Data.Map as M
 import System.Environment.XDG.BaseDir
 import XMonad
 import XMonad.Hooks.EwmhDesktops (ewmh, ewmhFullscreen)
-import XMonad.Hooks.ManageDocks (avoidStruts)
+import XMonad.Hooks.ManageDocks (AvoidStruts, avoidStruts, docks, ToggleStruts(..))
 import XMonad.Hooks.ManageHelpers
-import XMonad.Hooks.StatusBar (defToggleStrutsKey, statusBarProp, withEasySB)
-import XMonad.Hooks.StatusBar.PP
+import XMonad.Hooks.StatusBar (defToggleStrutsKey, statusBarGeneric, withEasySB)
+import XMonad.Hooks.TaffybarPagerHints (pagerHints)
+import XMonad.Layout.Decoration
 import XMonad.Layout.Magnifier (magnifiercz')
-import XMonad.Layout.NoBorders (Ambiguity (OnlyFloat, OtherIndicated), lessBorders, noBorders)
+import XMonad.Layout.NoBorders
 import XMonad.Layout.ThreeColumns (ThreeCol (ThreeColMid))
 import XMonad.Util.EZConfig (additionalKeysP)
-import XMonad.Util.Loggers (logTitles)
+
+withStrutsKey :: LayoutClass l Window => (XConfig Layout -> (KeyMask, KeySym)) -> XConfig l -> XConfig (ModifiedLayout AvoidStruts l)
+withStrutsKey key conf =
+  conf
+    { layoutHook = avoidStruts (layoutHook conf)
+    , keys = (<>) <$> keys' <*> keys conf
+    }
+ where
+  k' conf' = case key conf' of
+    (0, 0) ->
+      -- This usually means the user passed 'def' for the keybinding
+      -- function, and is otherwise meaningless to harmful depending on
+      -- whether 383ffb7 has been applied to xmonad or not. So do what
+      -- they probably intend.
+      --
+      -- A user who wants no keybinding function should probably use
+      -- 'withSB' instead, especially since NoSymbol didn't do anything
+      -- sane before 383ffb7. ++bsa
+      defToggleStrutsKey conf'
+    key' -> key'
+  keys' = (`M.singleton` sendMessage ToggleStruts) . k'
 
 main :: IO ()
 main = do
-  xmobar' <- getUserCacheFile "xmonad" "xmobar"
   xmonad
     . ewmhFullscreen
     . ewmh
-    . withEasySB (statusBarProp xmobar' (pure myXmobarPP)) defToggleStrutsKey
+    . docks
+    . withStrutsKey defToggleStrutsKey
+    . pagerHints
     $ myConfig
 
 myConfig =
@@ -56,32 +78,3 @@ myLayout = tiled ||| Mirror tiled ||| noBorders Full ||| threeCol
   nmaster = 1 -- Default number of windows in the master pane
   ratio = 1 / 2 -- Default proportion of screen occupied by master pane
   delta = 3 / 100 -- Percent of screen to increment by when resizing panes
-
-myXmobarPP :: PP
-myXmobarPP =
-  def
-    { ppSep = magenta " • "
-    , ppTitleSanitize = xmobarStrip
-    , ppCurrent = wrap " " "" . xmobarBorder "Top" "#8be9fd" 2
-    , ppHidden = white . wrap " " ""
-    , ppHiddenNoWindows = lowWhite . wrap " " ""
-    , ppUrgent = red . wrap (yellow "!") (yellow "!")
-    , ppOrder = \case [ws, l, _, wins] -> [ws, l, wins]; _ -> undefined
-    , ppExtras = [logTitles formatFocused formatUnfocused]
-    }
- where
-  formatFocused = wrap (white "[") (white "]") . magenta . ppWindow
-  formatUnfocused = wrap (lowWhite "[") (lowWhite "]") . blue . ppWindow
-
--- \| Windows should have *some* title, which should not not exceed a
--- sane length.
-ppWindow :: String -> String
-ppWindow = xmobarRaw . (\w -> if null w then "untitled" else w) . shorten 30
-
-blue, lowWhite, magenta, red, white, yellow :: String -> String
-magenta = xmobarColor "#ff79c6" ""
-blue = xmobarColor "#bd93f9" ""
-white = xmobarColor "#f8f8f2" ""
-yellow = xmobarColor "#f1fa8c" ""
-red = xmobarColor "#ff5555" ""
-lowWhite = xmobarColor "#bbbbbb" ""
