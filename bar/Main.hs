aa97fa490d9d0a1fd48ebdab66e67925c70c73d5
Trouble-Truffle <perigordtruffle7318@gmail.com>
2023-05-22 18:20:16 +0800

    feat(bar): Add mem graph and tray

--- bar/Main.hs
+++ bar/Main.hs
@@ -2,19 +2,30 @@
 
 module Main (main) where
 
-import Control.Monad.IO.Class (MonadIO (liftIO))
 import Data.Default (def)
+import GI.Gtk (Orientation (OrientationHorizontal), boxNew)
+import GI.Gtk qualified as GTK
+import GI.Gtk.Objects qualified as Gtk
+import GI.Gtk.Objects.Widget
+import System.Taffybar.Context (TaffyIO)
+import System.Taffybar.Hooks (getNetworkChan)
 import System.Taffybar.Information.CPU
-import System.Taffybar.Information.Network (getDeviceSamples)
 import System.Taffybar.SimpleConfig
 import System.Taffybar.Widget
 import System.Taffybar.Widget.Generic.Graph
 import System.Taffybar.Widget.Generic.PollingGraph
+import System.Taffybar.Information.Memory
 
 cpuCallback :: IO [Double]
 cpuCallback = do
   (_, systemLoad, totalLoad) <- cpuLoad
   return [totalLoad, systemLoad]
+
+
+memCallback :: IO [Double]
+memCallback = do
+  mi <- parseMeminfo
+  return [memoryUsedRatio mi]
 
 main :: IO ()
 main = do
@@ -23,14 +34,19 @@ main = do
           { graphDataColors = [(0, 1, 0, 1), (1, 0, 1, 0.5)]
           , graphLabel = Just "cpu"
           }
-      netCfg = def{graphLabel = Just "net"}
+      memCfg =
+        def
+          { graphDataColors = [(0.129, 0.588, 0.953, 1)]
+          , graphLabel = Just "mem"
+          }
       clock = textClockNewWith def
       cpu = pollingGraphNew cpuCfg 0.5 cpuCallback
-      network = networkGraphNew netCfg Nothing
+      mem = pollingGraphNew memCfg 0.5 memCallback
+      battery = textBatteryNew "$percentage$%"
       workspaces = workspacesNew def
       simpleConfig =
         def
           { startWidgets = [workspaces]
-          , endWidgets = [clock, sniTrayNew, cpu, network]
+          , endWidgets = reverse [cpu, mem, battery, clock, sniTrayNew]
           }
   simpleTaffybar simpleConfig
