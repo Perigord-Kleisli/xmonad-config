055134dd0a45fbe32b0a49ea4a451d352bfeda1b
Trouble-Truffle <perigordtruffle7318@gmail.com>
2023-07-20 20:55:06 +0800

    build: switch from haskell.nix to haskell-flake

--- flake.nix
+++ flake.nix
@@ -1,75 +1,68 @@
 {
-  description = "A very basic flake";
-  inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
-  inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-  outputs = {
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    haskell-flake.url = "github:srid/haskell-flake";
+  };
+  outputs = inputs @ {
     self,
     nixpkgs,
-    flake-utils,
-    haskellNix,
+    flake-parts,
+    ...
   }:
-    flake-utils.lib.eachSystem ["x86_64-linux"] (system: let
-      overlays = [
-        haskellNix.overlay
-        (final: prev: {
-          xmonad-config = final.haskell-nix.project' rec {
-            src = ./.;
-            index-state = "2023-01-21T20:07:39Z";
-            compiler-nix-name = "ghc925";
-            shell.tools = {
-              cabal = {inherit index-state;};
-              cabal-fmt = {inherit index-state;};
-              hlint = {inherit index-state;};
-              implicit-hie = {inherit index-state;};
-              haskell-language-server = {inherit index-state;};
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux"];
+      imports = [inputs.haskell-flake.flakeModule];
+
+      perSystem = {
+        self',
+        pkgs,
+        ...
+      }: {
+        # Typically, you just want a single project named "default". But
+        # multiple projects are also possible, each using different GHC version.
+        haskellProjects.default = {
+          # The base package set representing a specific GHC version.
+          # By default, this is pkgs.haskellPackages.
+          # You may also create your own. See https://haskell.flake.page/package-set
+          # basePackages = pkgs.haskellPackages;
+
+          # Extra package information. See https://haskell.flake.page/dependency
+          #
+          # Note that local packages are automatically included in `packages`
+          # (defined by `defaults.packages` option).
+          #
+          # packages = {
+          #   aeson.source = "1.5.0.0"; # Hackage version override
+          #   shower.source = inputs.shower;
+          # };
+          settings = {
+            X11 = {
+              extraBuildDepends = with pkgs.xorg; [libX11 libXrandr libXext libXScrnSaver libXinerama];
             };
-            shell.buildInputs = with pkgs; [
-              nixpkgs-fmt
-            ];
-            modules = [
-              {
-                packages.X11.components.library.libs = pkgs.lib.mkForce (with pkgs.xorg; [libX11 libXrandr libXext libXScrnSaver libXinerama]);
-                packages.gi-gtk-hs.components.library = {
-                  doHaddock = false;
-                };
-                packages.gi-gtk.components.library = {
-                  doHaddock = false;
-                };
-                packages.taffybar.components.library = {
-                  doHaddock = false;
-                };
-                packages.pango.components.library = {
-                  doHaddock = false;
-                  libs = pkgs.lib.mkForce (with pkgs; [pango glib pkg-config]);
-                };
-                packages.glib.components.library = {
-                  doHaddock = false;
-                  libs = pkgs.lib.mkForce (with pkgs; [glib pkg-config]);
-                };
-                packages.taffybar.components.library.libs = pkgs.lib.mkForce (with pkgs; [gtk4]);
-              }
-            ];
+            pango = {
+              haddock = false;
+              extraBuildDepends = with pkgs; [pango glib pkg-config];
+            };
+            glib = {
+              haddock = false;
+              extraBuildDepends = with pkgs; [glib pkg-config];
+            };
+            taffybar = {
+              haddock = false;
+              extraBuildDepends = with pkgs; [gtk4];
+            };
           };
-        })
-      ];
-      pkgs = import nixpkgs {
-        inherit system overlays;
-        inherit (haskellNix) config;
-      };
-      flake =
-        pkgs.xmonad-config.flake {
+
+          devShell = {
+            enable = true;
+
+            hlsCheck.enable = true;
+          };
         };
-    in
-      flake
-      // {
-        packages.default = flake.packages."xmonad-config:exe:xmonad-config";
-        packages.xmonad-config = flake.packages."xmonad-config:exe:xmonad-config";
-        packages.xmobar-config = flake.packages."xmonad-config:exe:xmobar-config";
-      });
-  nixConfig = {
-    extra-substituters = ["https://cache.iog.io"];
-    extra-trusted-public-keys = ["hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="];
-    allow-import-from-derivation = "true";
-  };
+
+        # haskell-flake doesn't set the default package, but you can do it here.
+        packages.default = self'.packages.xmonad-config;
+      };
+    };
 }
