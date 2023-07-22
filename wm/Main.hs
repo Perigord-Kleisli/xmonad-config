eb9b21f63e1d5a688a95cd6fb22b2d8deb510784
Trouble-Truffle <perigordtruffle7318@gmail.com>
2023-07-22 13:24:01 +0800

    feat: Add border gaps

--- wm/Main.hs
+++ wm/Main.hs
@@ -2,17 +2,19 @@
 
 module Main (main) where
 
+import System.Environment.XDG.BaseDir (getUserCacheFile)
 import XMonad
+import XMonad.Actions.WindowMenu (windowMenu)
 import XMonad.Hooks.EwmhDesktops (ewmh, ewmhFullscreen)
 import XMonad.Hooks.ManageDocks (avoidStruts, docks)
 import XMonad.Hooks.ManageHelpers
-import XMonad.Hooks.StatusBar (defToggleStrutsKey, withEasySB, statusBarGeneric)
+import XMonad.Hooks.StatusBar (defToggleStrutsKey, statusBarGeneric, withEasySB)
 import XMonad.Hooks.TaffybarPagerHints (pagerHints)
 import XMonad.Layout.Magnifier (magnifiercz')
 import XMonad.Layout.NoBorders
+import XMonad.Layout.Spacing
 import XMonad.Layout.ThreeColumns (ThreeCol (ThreeColMid))
 import XMonad.Util.EZConfig (additionalKeysP)
-import System.Environment.XDG.BaseDir (getUserCacheFile)
 
 main :: IO ()
 main = do
@@ -28,13 +30,17 @@ main = do
 myConfig =
   def
     { modMask = mod4Mask -- Rebind Mod to the Super key
-    , layoutHook = lessBorders OtherIndicated $ avoidStruts myLayout -- Use custom layouts
+    , layoutHook =
+        smartSpacingWithEdge 4
+          . lessBorders OtherIndicated
+          $ avoidStruts myLayout -- Use custom layouts
     , manageHook = myManageHook
     , terminal = "kitty"
-    , focusedBorderColor = "#ff5555"
-    , normalBorderColor = "#bbbbbb"
+    , focusedBorderColor = "#9496FF"
+    , normalBorderColor = "#636363"
     }
     `additionalKeysP` [ ("M-p", spawn "rofi -show drun")
+                      , ("M-o", windowMenu)
                       , ("M-S-s", spawn "flameshot gui")
                       , ("M-S-x", kill)
                       , ("<XF86AudioLowerVolume>", spawn "amixer -q sset Master 3%-")
@@ -49,7 +55,11 @@ myManageHook =
     , isDialog --> doFloat
     ]
 
-myLayout = tiled ||| Mirror tiled ||| noBorders Full ||| threeCol
+myLayout =
+  tiled
+    ||| Mirror tiled
+    ||| noBorders Full
+    ||| threeCol
  where
   threeCol = magnifiercz' 1.3 $ ThreeColMid nmaster delta ratio
   tiled = Tall nmaster delta ratio
